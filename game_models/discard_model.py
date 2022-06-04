4c7c1977a5229cc7c3e4df7aa1c0365968514207
marcjas <rhyor420@gmail.com>
2022-06-05 01:35:13 +0200

    discard nn wip

--- game_models/discard_model.py
+++ game_models/discard_model.py
@@ -15,41 +15,45 @@ tiles = [
     "wd", "gd", "rd"              
 ]
 
-BATCH_SIZE = 1000
+BATCH_SIZE = 100000 
 
 def main():
     dataset = DiscardDataset()
     dataloader =  DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(f"Using {device}...")
 
     net = Net().to(device)
     criterion = nn.CrossEntropyLoss()
 
     X_train, y_train = next(iter(dataloader))
+    X_train, y_train = X_train.to(device), y_train.to(device)
+
+    print(X_train.shape)
 
     learning_rate = 1e-1
     loss_list = []
-    for _ in tqdm(range(10)):
+    for _ in tqdm(range(1000)):
         y_pred = net(X_train)
         loss = criterion(y_pred, y_train)
+        print(loss.item())
         loss_list.append(loss.item())
-        net.zero_grad()
-        loss.backward()
+        net.zero_grad() # clear gradients of model parameters
+        loss.backward() # update weights
         with torch.no_grad():
             for param in net.parameters():
                 param -= learning_rate * param.grad
 
     logits = net(X_train)
-    pred_probab = nn.Softmax(dim=1)(logits)
-    y_pred = pred_probab.argmax(1)
-    print(len(y_pred))
-    print(y_pred[0])
+    pred_prob = nn.Softmax(dim=1)(logits)
+    y_pred = torch.argmax(pred_prob, dim=1)
+
     wrong_cnt = 0
     for i in range(len(y_pred)):
         label = tiles[(y_train[i] == 1).nonzero(as_tuple=True)[0]]
         predicted_tile = tiles[y_pred[i]]
-        print(f"predicted: {predicted_tile}, actual discard: {label}")
+        # print(f"predicted: {predicted_tile}, actual discard: {label}")
         if label != predicted_tile:
             wrong_cnt += 1
     accuracy = 100 - (100*(wrong_cnt / len(y_train)))
@@ -59,21 +63,25 @@ def main():
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(4923, 3000),
+
+        self.c1 = nn.Sequential(
             nn.ReLU(),
-            nn.Linear(3000, 2000),
+        )
+
+        in_features = 4923
+
+        self.l1 = nn.Sequential(
+            nn.Linear(in_features, in_features*2),
             nn.ReLU(),
-            nn.Linear(2000, 1000),
+            nn.Linear(in_features*2, in_features*2),
             nn.ReLU(),
-            nn.Linear(1000, 34),
+            nn.Linear(in_features*2, 34),
         )
 
     def forward(self, x):
-        logits = self.linear_relu_stack(x)
-        return logits
-
-
+        # x = x.view(1000, 4923, 1)
+        # x = self.c1(x)
+        return self.l1(x)
 
 if __name__ == "__main__":
     main()
