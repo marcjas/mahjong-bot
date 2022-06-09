abaa871fb679bfb7bef935c8f324a114a142e5b6
marcjas <rhyor420@gmail.com>
2022-06-10 01:52:51 +0200

    lol

--- game_models/datasets.py
+++ game_models/datasets.py
@@ -11,6 +11,7 @@ discard_data_file_path = "../data/model_data/discard_data.dat"
 
 class ChiiDataset(Dataset):
     def __init__(self, size=None):
+        print("Loading data...")
         self.len = Utils.get_metadata()[0] if size == None else size
         self.data = torch.FloatTensor(torch.FloatStorage.from_file(chii_data_file_path, shared=False, size=self.len* 4924)).reshape(self.len, 4924)
         print(f"Loaded {self.len} samples...")
@@ -23,6 +24,7 @@ class ChiiDataset(Dataset):
 
 class PonDataset(Dataset):
     def __init__(self, size=None):
+        print("Loading data...")
         self.len = Utils.get_metadata()[1] if size == None else size
         self.data = torch.FloatTensor(torch.FloatStorage.from_file(pon_data_file_path, shared=False, size=self.len * 4924)).reshape(self.len, 4924)
         print(f"Loaded {self.len} samples...")
@@ -35,6 +37,7 @@ class PonDataset(Dataset):
 
 class KanDataset(Dataset):
     def __init__(self, size=None):
+        print("Loading data...")
         self.len = Utils.get_metadata()[2] if size == None else size
         self.data = torch.FloatTensor(torch.FloatStorage.from_file(kan_data_file_path, shared=False, size=self.len * 4924)).reshape(self.len, 4924)
         print(f"Loaded {self.len} samples...")
@@ -47,6 +50,7 @@ class KanDataset(Dataset):
 
 class RiichiDataset(Dataset):
     def __init__(self, size=None):
+        print("Loading data...")
         self.len = Utils.get_metadata()[3] if size == None else size
         self.data = torch.FloatTensor(torch.FloatStorage.from_file(riichi_data_file_path, shared=False, size=self.len * 4924)).reshape(self.len, 4924)
         print(f"Loaded {self.len} samples...")
@@ -59,6 +63,7 @@ class RiichiDataset(Dataset):
 
 class DiscardDataset(Dataset):
     def __init__(self, size=None):
+        print("Loading data...")
         self.len = Utils.get_metadata()[4] if size == None else size
         self.data = torch.FloatTensor(torch.FloatStorage.from_file(discard_data_file_path, shared=False, size=self.len * 4924)).reshape(self.len, 4924)
         print(f"Loaded {self.len} samples...")
--- game_models/discard_model.py
+++ game_models/discard_model.py
@@ -6,36 +6,31 @@ import torch.nn as nn
 from tqdm import tqdm
 import sys
 import wandb
+import time
+import os
 
-tiles = [
-    "1m", "2m", "3m", "4m", 
-    "5m", "6m", "7m", "8m", "9m",
-    "1p", "2p", "3p", "4p", 
-    "5p", "6p", "7p", "8p", "9p", 
-    "1s", "2s", "3s", "4s", 
-    "5s", "6s", "7s", "8s", "9s", 
-    "ew", "sw", "ww", "nw",
-    "wd", "gd", "rd"
-]
-
-USE_WANDB = True
-SAVE_INTERVAL = None # set to None if you don't want to torch.save(), other every x epochs
-TRAIN_DATASET_SIZE = 20000
-TEST_DATASET_SIZE = 5000
-BATCH_SIZE = 8000 
+# config
+USE_WANDB = False
+SAVE_INTERVAL = None # set to None if you don't want to torch.save()
+SAVE_PATH = "D:/models"
+RNG_SEED = None
+TRAIN_DATASET_SIZE = 1000
+TEST_DATASET_SIZE = 100
+BATCH_SIZE = 1000
 
 # hyperparams
 LEARNING_RATE = 1
 EPOCHS = 200
 
-if USE_WANDB: wandb.init(project="riichi-mahjong", entity="shuthus")
+if USE_WANDB: wandb.init(project="riichi-mahjong", entity="shuthus", tags=["discard"])
 
 def main():
-    if TRAIN_DATASET_SIZE is not None and BATCH_SIZE > TRAIN_DATASET_SIZE:
-        print("BATCH_SIZE can't be smaller than DATASET_SIZE")
+    if TRAIN_DATASET_SIZE and BATCH_SIZE > TRAIN_DATASET_SIZE:
+        print("BATCH_SIZE can't be bigger than TRAIN_DATASET_SIZE")
         sys.exit(-1)
 
-    # total_size = 
+    if RNG_SEED: torch.manual_seed(RNG_SEED)
+
     dataset = DiscardDataset(TRAIN_DATASET_SIZE + TEST_DATASET_SIZE)
     train_dataset, test_dataset = random_split(dataset, (TRAIN_DATASET_SIZE, TEST_DATASET_SIZE))
     train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
@@ -43,7 +38,7 @@ def main():
     print(f"Train size: {len(train_dataset)}, test size: {len(test_dataset)}")
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    print(f"Using {device}...")
+    print(f"Using {device}")
 
     net = Net().to(device)
     criterion = nn.CrossEntropyLoss()
@@ -52,7 +47,16 @@ def main():
     train_n_size = len(train_dataset)
     test_n_size = len(test_dataset)
 
-    for epoch in tqdm(range(EPOCHS)):
+    if USE_WANDB: wandb.log({
+        "Epoch": 0,
+        "Train loss": 0,
+        "Train acc": 0,
+        "Test acc": 0, 
+    })
+
+    timestamp = int(time.time())
+
+    for epoch in tqdm(range(1, EPOCHS + 1)):
         total_epoch_train_loss = 0
         train_correct = 0
         for x, y in train_dataloader:
@@ -70,31 +74,30 @@ def main():
             predictions = torch.argmax(nn.Softmax(dim=1)(y_pred), dim=1)
             train_correct += (predictions == y_train).float().sum()
 
-        test_correct = 0
-        for x, y in test_dataloader:
-            X_test, y_test = x.to(device), y.squeeze().to(device)
-            y_pred = net(X_test)
+        if epoch % 10 == 0 or epoch == EPOCHS:
+            test_correct = 0
+            for x, y in test_dataloader:
+                X_test, y_test = x.to(device), y.squeeze().to(device)
+                y_pred = net(X_test)
 
-            predictions = torch.argmax(nn.Softmax(dim=1)(y_pred), dim=1)
-            test_correct += (predictions == y_test).float().sum()
+                predictions = torch.argmax(nn.Softmax(dim=1)(y_pred), dim=1)
+                test_correct += (predictions == y_test).float().sum()
 
-        if USE_WANDB: wandb.log({
-            "Epoch": epoch,
-            "Train loss": total_epoch_train_loss / train_n_batches,
-            "Train acc": 100 * (train_correct / train_n_size),
-            "Test acc": 100 * (test_correct / test_n_size),
-        })
+            if USE_WANDB: wandb.log({
+                "Epoch": epoch,
+                "Train loss": total_epoch_train_loss / train_n_batches,
+                "Train acc": 100 * (train_correct / train_n_size),
+                "Test acc": 100 * (test_correct / test_n_size),
+            })
 
-        if SAVE_INTERVAL is not None and (epoch % SAVE_INTERVAL == 0 or epoch == EPOCHS - 1):
-            torch.save(net.state_dict(), f"D:/models/discard_model_{epoch}")
+        if SAVE_INTERVAL and (epoch % SAVE_INTERVAL == 0 or epoch == EPOCHS):
+            fn = f"{SAVE_PATH}/discard_{timestamp}_{epoch}.pt"
+            torch.save(net.state_dict(), fn)
+            print(f"Saved model {fn} with size {os.path.getsize(fn)}")
 
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-
-        self.c1 = nn.Sequential(
-            nn.ReLU(),
-        )
 
         in_features = 4923
         self.l1 = nn.Sequential(
@@ -105,6 +108,29 @@ class Net(nn.Module):
 
     def forward(self, x):
         return self.l1(x)
+
+class DiscardModel:
+    def __init__(self, model_path):
+        self.model = Net().load_state_dict(torch.load(model_path))
+
+    def predict(self, X):
+        X = torch.FloatTensor(X)
+        pred = self.model(X)
+        pred_probab = nn.Softmax(dim=1)(pred)
+        y_pred = pred_probab.argmax(1)
+        return tiles[y_pred]
+
+
+tiles = [
+    "1m", "2m", "3m", "4m", 
+    "5m", "6m", "7m", "8m", "9m",
+    "1p", "2p", "3p", "4p", 
+    "5p", "6p", "7p", "8p", "9p", 
+    "1s", "2s", "3s", "4s", 
+    "5s", "6s", "7s", "8s", "9s", 
+    "ew", "sw", "ww", "nw",
+    "wd", "gd", "rd"
+]
 
 if __name__ == "__main__":
     main()
